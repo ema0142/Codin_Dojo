f6cc42ac4da909f40e7d0052e14995fc1f91faa7
emna0142 <emnamajdii35@gmail.com>
2023-10-17 15:02:45 +0100

    day 1 commit

---
+++ Programming Basics/Week 1/Day 1/Optional/code.js
@@ -0,0 +1,12 @@
+//variable to store likes
+var likeCount = 0;
+// foction for increasing the likes
+function increaseLikes() {
+//adding one to likecount 
+    likeCount = likeCount + 1;
+}
+
+
+
+
+
---
+++ Programming Basics/Week 1/Day 1/Practice/level 5.js
@@ -0,0 +1,31 @@
+//challenge number 1
+var x = 5;
+console.log(x);
+//5
+
+//challenge number 2
+var x = 3;
+x=x+1;
+console.log(x)
+//4
+
+//challenge number 3
+var x = 3;
+x=x+2;
+console.log(x+2);
+console.log(x);
+//7,5
+
+//challenge number 4
+var x = 3;
+x = x*x;
+console.log(x);
+console.log(x*2);
+//9,18
+
+//challenge number 5
+var x = 2;
+var y = 5;
+var z = x+y;
+console.log(z)
+//7
