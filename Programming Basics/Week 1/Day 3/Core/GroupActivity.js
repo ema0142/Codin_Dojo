cd4d20de6c12f7ddd176ba24e4d377a5c08df38d
emna0142 <emnamajdii35@gmail.com>
2023-10-23 14:00:07 +0100

    commit

---
+++ Programming Basics/Week 1/Day 3/Core/GroupActivity.js
@@ -0,0 +1,15 @@
+//Dhia/Emna/Ele
+var decade=0;
+for (var i=1996; i<2023; i++)
+{  // x++; > x=x+1; > x+=1;
+    if (decade==10){
+        console.log("happy decade of JS!");
+        decade=0;
+    }
+    else {console.log(i+" JS is awesome!");
+}
+if ( i == 2022){
+    console.log("HAPPY 27 years of JAVASCRIPT! 🎈🎈🎈");
+}
+decade++;
+}
