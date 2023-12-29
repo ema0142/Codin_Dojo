3af0c274fbf1086d0d38da05fefff8f05a890066
emna0142 <emnamajdii35@gmail.com>
2023-12-29 18:47:56 +0100

    faker

---
+++ MERNStack/week_2/Day_3/Core/Faker API/server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { faker } = require('@faker-js/faker');
+const app = express();
+const port = 8000; 
+
+
+const createUser = () => {
+    return {
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+        phoneNumber: faker.phone.phoneNumber(),
+        lastName: faker.name.lastName(),
+        firstName: faker.name.firstName(),
+        _id: faker.datatype.uuid(),
+    };
+};
+
+const createCompany = () => {
+    return {
+        _id: faker.datatype.uuid(),
+        name: faker.company.companyName(),
+        address: {
+            street: faker.address.streetAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country(),
+        },
+    };
+};
+
+app.get('/api/users/new', (req, res) => {
+    const newUser = createUser();
+    res.json(newUser);
+});
+
+app.get('/api/companies/new', (req, res) => {
+    const newCompany = createCompany();
+    res.json(newCompany);
+});
+
+app.get('/api/user/company', (req, res) => {
+    const newUser = createUser();
+    const newCompany = createCompany();
+    res.json({ user: newUser, company: newCompany });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
