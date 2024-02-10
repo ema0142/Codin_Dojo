4760a4634facfc9d37efacd4e564b4d621aed94c
emna0142 <emnamajdii35@gmail.com>
2024-02-10 19:15:05 +0100

    Daikichi

---
+++ Java/Week1/Day5/Core/DikichiPath/src/main/java/com/emna/beltreview/DikichiPathApplication.java
@@ -0,0 +1,13 @@
+package com.emna.beltreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DikichiPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DikichiPathApplication.class, args);
+	}
+
+}
